04e9e57835b41a495e1484d2e6db0f578ad174c6
elvinciqueira <elvinciqueira@gmail.com>
2020-02-02 15:04:30 -0200

    play, pause and stop

---
+++ custom-video-player/script.js
@@ -0,0 +1,49 @@
+const video = document.querySelector('#video');
+const play = document.querySelector('#play');
+const stop = document.querySelector('#stop');
+const progress = document.querySelector('#progress');
+const timestamp = document.querySelector('#timestamp');
+
+//Play & pause video
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+//update play/pause icon
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+}
+
+//update progress & timestamp
+function updateProgress() {
+  return true;
+}
+
+//set video time to progress
+function setVideoProgress() {
+  return true;
+}
+
+function stopVideo() {
+  video.currentTime = 0;
+  video.pause();
+}
+
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+
+stop.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress);
