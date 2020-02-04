4a053d740bb638a8ea92a52ea7c60d67910449b2
elvinciqueira <elvinciqueira@gmail.com>
2020-02-03 23:15:22 -0200

    Breath animation with JS Trigger

---
+++ relaxer-app/script.js
@@ -0,0 +1,26 @@
+const container = document.querySelector('#container');
+const text = document.querySelector('#text');
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+breathAnimation();
+
+function breathAnimation() {
+  text.innerText = 'Breathe in!';
+
+  container.className = 'container grow';
+
+  setTimeout(() => {
+    text.innerText = 'Hold';
+
+    setTimeout(() => {
+      text.innerText = 'Breathe Out!';
+
+      container.className = 'container shrink';
+    }, holdTime);
+  }, breatheTime);
+}
+
+setInterval(breathAnimation, totalTime);
