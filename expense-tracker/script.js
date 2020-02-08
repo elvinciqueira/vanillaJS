770fd2f26e7941dab073d83193887dde6fc8081b
elvinciqueira <elvinciqueira@gmail.com>
2020-02-08 16:24:07 -0200

    show transaction items, display balance, income & expense

---
+++ expense-tracker/script.js
@@ -0,0 +1,65 @@
+const balance = document.querySelector('#balance');
+const moneyPlus = document.querySelector('#money-plus');
+const moneyMinus = document.querySelector('#money-minus');
+const list = document.querySelector('#list');
+const form = document.querySelector('#form');
+const text = document.querySelector('#text');
+const amount = document.querySelector('#amount');
+
+const dummyTransactions = [
+  { id: 1, text: 'Flower', amount: -20 },
+  { id: 2, text: 'Salary', amount: 300 },
+  { id: 3, text: 'Book', amount: -10 },
+  { id: 4, text: 'Camera', amount: 150 }
+];
+
+let transactions = dummyTransactions;
+
+const addTransactionDOM = transaction => {
+  const sign = transaction.amount < 0 ? '-' : '+';
+
+  const itemElement = document.createElement('li');
+
+  const classValue = transaction.amount < 0 ? 'minus' : 'plus';
+  itemElement.classList.add(classValue);
+
+  itemElement.innerHTML = `
+    ${transaction.text} 
+      <span>
+        ${sign}${Math.abs(transaction.amount)}
+      </span>
+      <button class="delete-btn">x</button>
+  `;
+
+  list.appendChild(itemElement);
+};
+
+const updateValues = () => {
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  balance.innerText = `$${total}`;
+  moneyPlus.innerText = `$${income}`;
+  moneyMinus.innerText = `$${expense}`;
+};
+
+const init = () => {
+  list.innerHTML = '';
+
+  transactions.forEach(addTransactionDOM);
+
+  updateValues();
+};
+
+init();
