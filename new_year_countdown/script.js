e53520e1343a4d7b96d33b7e3c50c9dc799166ff
elvinciqueira <elvinciqueira@gmail.com>
2020-02-24 22:26:01 -0300

    create the countdown

---
+++ new_year_countdown/script.js
@@ -0,0 +1,25 @@
+const daysElement = document.querySelector('#days');
+const hoursElement = document.querySelector('#hours');
+const minutesElement = document.querySelector('#minutes');
+const secondsElement = document.querySelector('#seconds');
+const countdownElement = document.querySelector('#countdown');
+
+const currentYear = new Date().getFullYear();
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+function updateCountdown() {
+  const currentTime = new Date();
+  const difference = newYearTime - currentTime;
+
+  const days = Math.floor(difference / 1000 / 3600 / 24);
+  const hours = Math.floor(difference / 1000 / 3600) % 24;
+  const minutes = Math.floor(difference / 1000 / 60) % 60;
+  const seconds = Math.floor(difference / 1000) % 60;
+
+  daysElement.innerHTML = days;
+  hoursElement.innerHTML = hours < 10 ? '0' + hours : hours;
+  minutesElement.innerHTML = minutes < 10 ? '0' + minutes : minutes;
+  secondsElement.innerHTML = seconds < 10 ? '0' + seconds : seconds;
+}
+
+setInterval(updateCountdown, 1000);
